3c2acef6b31c08e9d81dd83e3fac0f377e7bf9ef
Rezial011 <dafaalfarezi27@gmail.com>
2025-02-21 20:55:13 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { React } from 'react'
+import './index.css'
+import Data from './components/Data'
+import HomeLayout from './components/home/HomeLayout'
+
+function App() {
+
+  return (
+    <>
+      <HomeLayout/>
+      {/* <Data/> */}
+    </>
+  )
+}
+
+export default App
